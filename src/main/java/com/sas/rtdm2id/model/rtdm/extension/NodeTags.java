2912ac0de0da966427b6eb76f3db19c2a695488a
markuscsaba <MarkusCsab@ext.otpbank.hu>
2023-12-13 08:04:25 +0100

    Initial commit

---
+++ src/main/java/com/sas/rtdm2id/model/rtdm/extension/NodeTags.java
@@ -0,0 +1,42 @@
+/*
+Copyright © 2023, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+*/
+package com.sas.rtdm2id.model.rtdm.extension;
+
+import javax.xml.bind.annotation.*;
+import java.io.Serializable;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+        "value"
+})
+public class NodeTags implements Serializable {
+    private final static long serialVersionUID = -1L;
+    
+    @XmlValue
+    protected String value;
+    @XmlAttribute(name = "EmptyList")
+    protected String emptyList;
+
+       
+    public String getValue() {
+        return value;
+    }
+
+       
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+       
+    public String getEmptyList() {
+        return emptyList;
+    }
+
+       
+    public void setEmptyList(String value) {
+        this.emptyList = value;
+    }
+
+}
